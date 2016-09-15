4eac63dd81ce34cc3504c7cd852a4d38fc52cdbc
Loïs_MBP <lois.drouglazet@gmail.com>
2016-09-15 11:32:20 +0200

    add some folder and code

--- test.php
+++ test.php
@@ -1,4 +1,33 @@
-<?php
-if (extension_loaded ('SimpleXML')) echo 'SimpleXML peut être utilisé'; 
-else echo 'SimpleXML ne peut pas être utilisé : je ferais mieux de changer d\'hébergeur';
-?>
+<!DOCTYPE html>
+<HTML>
+<HEAD>
+<meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<TITLE>Entre_Cabanes</TITLE>
+
+</HEAD>
+
+<BODY>
+
+
+
+<ul>
+<?php 
+      foreach(glob('SITE/AILLEUR/*', GLOB_ONLYDIR) as $filename){
+      $rest = substr($filename, 13);    
+      echo "<li><a href='$filename'>".$rest."</a></li>";
+      }
+    ?>
+</ul>
+
+
+</BODY>
+
+
+</HTML>
+    
+
+
+
